c8c9b5c1073c2f62729b6bfdfe724a9c0ef491b3
Will Lu <zeren828@gmail.com>
2017-06-02 16:53:54 +0800

    dev

--- application/controllers/votes/Demos.php
+++ application/controllers/votes/Demos.php
@@ -3,6 +3,13 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Demos extends CI_Controller
 {
+	// 投票標題
+	private $votes_title = '投票範例';
+	// 投票資料庫使用表單
+	private $votes_table = 'vote_mrplay_tbl';
+	// 投票統計資料庫使用表單
+	private $votes_list_table = 'vote_mrplay_list_tbl';
+	// 回傳資料
 	private $data_view;
 
 	function __construct ()
@@ -14,6 +21,10 @@ class Demos extends CI_Controller
 
 	public function index ()
 	{
-		echo 'index';
+		echo '投票首頁';
+		echo $this->votes_title;
+		echo $this->votes_table;
+		echo $this->votes_list_table;
+		echo $this->data_view;
 	}
 }
