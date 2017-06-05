e5c6a9e60585e9748552582bd15d3cab59e2378a
Will Lu <zeren828@gmail.com>
2017-06-05 16:49:45 +0800

    dev

--- application/controllers/lottery/Events.php
+++ application/controllers/lottery/Events.php
@@ -26,24 +26,26 @@ class Events extends CI_Controller {
 		//取得mongo會員
 		$this->mongo_db->limit(200);
 		$this->mongo_db->offset(1);
+		// 開始時間
+		$data_date ['start_time'] = strtotime ( $date . "-1 hour" );
+		$data_date ['start'] = date ( "Y-m-d 00:00:00", $data_date ['start_time'] );
+		$data_date ['start_utc'] = date ( "Y-m-d H:i:s", strtotime ( $data_date ['start'] . "-8 hour" ) );
+		$data_date ['start_mongo'] = new MongoDate(strtotime($data_date ['start_utc']));
+		// 七天後
+		$data_date ['end_time'] = strtotime ( $data_date ['start'] . "+7 day" );
+		$data_date ['end'] = date ( "Y-m-d 00:00:00", $data_date ['end_time'] );
+		$data_date ['end_utc'] = date ( "Y-m-d H:i:s", strtotime ( $data_date ['end'] . "-8 hour" ) );
+		$data_date ['end_mongo'] = new MongoDate(strtotime($data_date ['end_utc']));
+		//var_dump($data_date);
 		if(!empty($date)){
-			// 開始時間
-			$data_date ['start_time'] = strtotime ( $date . "-1 hour" );
-			$data_date ['start'] = date ( "Y-m-d 00:00:00", $data_date ['start_time'] );
-			$data_date ['start_utc'] = date ( "Y-m-d H:i:s", strtotime ( $data_date ['start'] . "-8 hour" ) );
-			$data_date ['start_mongo'] = new MongoDate(strtotime($data_date ['start_utc']));
-			// 七天後
-			$data_date ['end_time'] = strtotime ( $data_date ['start'] . "+7 day" );
-			$data_date ['end'] = date ( "Y-m-d 00:00:00", $data_date ['end_time'] );
-			$data_date ['end_utc'] = date ( "Y-m-d H:i:s", strtotime ( $data_date ['end'] . "-8 hour" ) );
-			$data_date ['end_mongo'] = new MongoDate(strtotime($data_date ['end_utc']));
-			//var_dump($data_date);
 			//
 			$this->mongo_db->where_between('_created_at', $data_date ['start_mongo'], $data_date ['end_mongo']);
 		}
 		$user = $this->mongo_db->select(array('_id', 'member_id'))->get('_User');
 		//var_dump($user);
 		$this->data_view['tag'] = $tag;
+		$this->data_view['start'] = $data_date ['start'];
+		$this->data_view['end'] = $data_date ['end'];
 		$this->data_view['lottery'] = $user;
 		// 輸出view
 		$this->load->view ( 'lottery/event_2017_1', $this->data_view );
